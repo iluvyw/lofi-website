d9b2867e439c8fec0420ab5f0729bc182d61e65a
An Pham <anhoangpham2001@gmail.com>
2022-05-22 10:19:21 +0700

    Db

--- client/src/components/Background.jsx
+++ client/src/components/Background.jsx
@@ -15,7 +15,7 @@ export default function Background({background}) {
     return (
         <div className="relative w-full h-full flex flex-row overflow-hidden">
             <h3 onClick={() => prevIndex()} className="absolute font-extralight top-1/2 left-6 z-50 -rotate-90 uppercase cursor-pointer">Prev</h3>
-            {background.map(item => <img src={item} alt="bg" className="relative w-full h-full flex-shrink-0 object-contain duration-500" style={{ transform: `translate3d(${-index * 100}%, 0, 0)` }} />)}
+            {background.map((item,i) => <img key={i} src={item} alt="bg" className="relative w-full h-full flex-shrink-0 object-contain duration-500" style={{ transform: `translate3d(${-index * 100}%, 0, 0)` }} />)}
             <h3 onClick={() => nextIndex()} className="absolute font-extralight top-1/2 right-6 z-50 rotate-90 uppercase cursor-pointer">Next</h3>
         </div>
     )
